d918a9fea741de6c48fcf585e19b14f6e5671fcf
Derek Castillo <castilloderek445@gmail.com>
2023-10-21 14:37:10 -0500

    Initial Commit

---
+++ ubiqcomp_semester_project_beta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ubiqcomp_semester_project_beta
+//
+//  Created by Derek Castillo on 10/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ubiqcomp_semester_project_beta/ubiqcomp_semester_project_betaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ubiqcomp_semester_project_betaApp.swift
+//  ubiqcomp_semester_project_beta
+//
+//  Created by Derek Castillo on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ubiqcomp_semester_project_betaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
