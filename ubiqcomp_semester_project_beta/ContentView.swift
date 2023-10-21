5713cbd4506c94dbced87d071e1ca85fcc0a8063
Derek Castillo <castilloderek445@gmail.com>
2023-10-21 14:51:14 -0500

    edits to contentview as practice on committing in branch

--- ubiqcomp_semester_project_beta/ContentView.swift
+++ ubiqcomp_semester_project_beta/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("edits made from Derek Branch")
         }
         .padding()
     }
